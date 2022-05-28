ee974916626a9a4fc18516c918835fffe56695c9
Diogenes4Sinope <talk2nategwu@protonmail.com>
2022-05-28 14:08:13 +0000

    Fixed main class for mservice

--- main-service/m-service/src/main/java/com/minejava/mservice/MServiceApplication.java
+++ main-service/m-service/src/main/java/com/minejava/mservice/MServiceApplication.java
@@ -1,4 +1,11 @@
 package com.minejava.mservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MServiceApplication.class, args);
+    }
 }
