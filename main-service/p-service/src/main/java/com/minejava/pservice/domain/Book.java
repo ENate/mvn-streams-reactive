278d8e1495f8c5528291780776784d443d324117
Nathaniel Egwu <nathaniel@aims.ac.za>
2022-08-29 11:04:17 +0000

    Fixed long connection via repo

--- main-service/p-service/src/main/java/com/minejava/pservice/domain/Book.java
+++ main-service/p-service/src/main/java/com/minejava/pservice/domain/Book.java
@@ -1,8 +1,6 @@
 package com.minejava.pservice.domain;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
-import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Column;
 
 import lombok.AllArgsConstructor;
